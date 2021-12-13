359a91435eae34bfdc6d728540628acf073963d3
cryptofluencerr <kcbizzkc@gmail.com>
2021-12-13 18:14:29 +0530

    update

--- src/github.com/cryptofluencerr/firstapp/main.go
+++ src/github.com/cryptofluencerr/firstapp/main.go
@@ -2,29 +2,34 @@ package main
 
 import (
 	"fmt"
-	"time"
+	// "time"
 )
 
-func pinger(c chan string) {
-  for i := 0; ; i++ {
-    c <- "ping"
-  }
-}
+// func pinger(c chan string) {
+//   for i := 0; ; i++ {
+//     c <- "ping"
+//   }
+// }
 
-func printer(c chan string) {
-  for {
-    msg := <- c
-    fmt.Println(msg)
-    time.Sleep(time.Second * 1)
-  }
-}
+// func printer(c chan string) {
+//   for {
+//     msg := <- c
+//     fmt.Println(msg)
+//     time.Sleep(time.Second * 1)
+//   }
+// }
 
 func main() {
-  var c chan string = make(chan string)
+	// var c chan string = make(chan string)
 
-  go pinger(c)
-  go printer(c)
+	// go pinger(c)
+	// go printer(c)
 
-  var input string
-  fmt.Scanln(&input)
+	// var input string
+	// fmt.Scanln(&input)
+	var j int = 27
+	i := 42
+	fmt.Printf("%v, %T", i, i)
+  fmt.Print("\n")
+	fmt.Printf("%v, %T", j, j)
 }
