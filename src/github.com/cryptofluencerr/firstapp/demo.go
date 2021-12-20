1c563d0a565fb6fdc5ee71cb8d06e5ee33acfce3
cryptofluencerr <kcbizzkc@gmail.com>
2021-12-20 18:43:42 +0530

    update

--- src/github.com/cryptofluencerr/firstapp/demo.go
+++ src/github.com/cryptofluencerr/firstapp/demo.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 //if the declaration is in capital letters then it is accessible to all the packages
 // var I int = 27
 
@@ -44,8 +42,6 @@ func main() {
 
 	// fmt.Println("%0.4fTB", fileSize/TB)
 
-	//
-
 	// var a = [...]int{1, 2, 3, 4, 5}
 	// b := append(a[:1], a[2:]...)
 	// fmt.Println(b)
@@ -79,22 +75,77 @@ func main() {
 	// fmt.Println(anotherDoctor)
 
 	//EMBEDDING IN STRUCT
+	// type Animal struct {
+	// 	origin string
+	// 	name   string
+	// }
+	// type Bird struct {
+	// 	Animal
+	// 	speed  float32
+	// 	canFly bool
+	// }
 
-	type Animal struct {
-		origin string
-		name   string
-	}
-	type Bird struct {
-		Animal
-		speed  float32
-		canFly bool
-	}
+	// b := Bird{speed: 34.4,
+	// 	canFly: true,
+	// 	Animal: Animal{name: "Emu",
+	// 		origin: "Australia"}}
+	// fmt.Println(b)
 
-	b := Bird{speed: 34.4,
-		canFly: true,
-		Animal: Animal{name: "Emu",
-			origin: "Australia"}}
+	//IF-ELSE
 
-	fmt.Println(b)
+	// statePopulations := map[string]int{
+	// 	"California":   1232132131,
+	// 	"Texas":        23242432,
+	// 	"Florida":      231312313,
+	// 	"New York":     31323432,
+	// 	"Pennsylvania": 34354645,
+	// }
+	// if pop, ok := statePopulations["Florida"]; ok {
+	// 	fmt.Println(pop)
+	// }
+	// number := 50
+	// guess := 1
+	// if guess < 1 || guess > 100 {
+	// 	fmt.Println("Guess must be between 1 and 100!")
+	// }
 
+	// if guess < number {
+	// 	fmt.Println("Too low")
+	// }
+	// if guess > number {
+	// 	fmt.Println("Too high")
+	// }
+	// if guess == number {
+	// 	fmt.Println("You got it!!")
+	// }
+	// myNum := 0.123
+	// if math.Abs(myNum/math.Pow(math.Sqrt(myNum), 2)-1) < 0.001 {
+	// 	fmt.Println(math.Abs(myNum/math.Pow(math.Sqrt(myNum), 2) - 1))
+	// 	fmt.Println("These are same")
+	// } else {
+	// 	fmt.Println("These are not same")
+	// }
+
+	// SWITCH STATEMENTS
+
+	// rand.Seed(time.Now().UnixNano())
+	// fmt.Println(rand.Intn(10))
+	// switch rand.Intn(10) {
+	// case 1, 3, 5, 7, 9:
+	// 	fmt.Println("one, three, five, seven, nine")
+	// case 2, 4, 6, 8:
+	// 	fmt.Println("two, four, six, eight")
+	// default:
+	// 	fmt.Println("10")
+	// }
+	// i := 10
+	// switch {
+	// case i <= 10:
+	// 	fmt.Println("less than or equal to ten")
+	// case i <= 20:
+	// 	fmt.Println("less than or equal to twenty")
+	// default:
+	// 	fmt.Println("greater than twenty")
+	// }
+	//3:12:43
 }
